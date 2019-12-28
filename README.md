2c3dd68bab5a80ae2929c9112e5f7686bb56f95a
Seth Stradling <seth.g.stradling@gmail.com>
2019-12-28 13:38:16 -0700

    first draft of readme

--- README.md
+++ README.md
@@ -21,11 +21,11 @@ const { Portal } = require('botkit-portal-slack');
 Configure the plugin with the URI of your app instance, and the portal token and client signing secret you'll recieve when you install the Portal app.
 
 The plugin defaults to listening for the following commands:
- /<your_app_slash_command> [feedback | support] <any additional text here>
- /[feedback | support] (requires configuring corresponding slash commands for your app [here](https://url_for_app_config)
- @<app name> [support | help | feedback]
+ /<your_app_slash_command> [feedback | support | <other specified type>] <any additional text here> : launches our feedback modal, prepopulated with any additional text included in the command. The modal also includes a dropdown populated with your pre-specified request types.
+ /[feedback | support | <other specified type>] <any additional text>: this requires configuring corresponding slash commands for your app at https://api.slack.com/apps/<your_app_id>/slash-commands. 
+ @<app name> [support | help | feedback| <other specified type>] <any additional text here>: we don't support free-text @mentions - like 'hey @<app>, can I get some help?' yet. @mentions cannot directly 
 
-if you want to trigger the support dialog/modal from an IM action, Portal listens for ```im_portal_launch``` by default as well. If the event associated with an IM keyword also contains text, Portal will pass that text into the pop-up text field, so you can pre-populate feedback modals
+if you want to trigger the support dialog/modal from an IM or BlockKit action, Portal listens for ```action_portal_launch[:<type>]``` callbacks by default. If you want to be able to specify a type (e.g.: 'Send us feedback' v. 'Make a support request'), you can append a type to the end of your callback_id. You can also use this pattern for message actions (configure at https://api.slack.com/apps/<your_app_id>/interactive-messages) if you'd like to use them.
 
 You can configure specific slash ('/') and at ('@') commands for your bot using the following format: 
 ```{
@@ -36,25 +36,50 @@ You can configure specific slash ('/') and at ('@') commands for your bot using 
                 <another command>: [] //an empty list tells portal to respond to the /command directly. This allows it to pull all text from the command as content - e.g.: /support I have a major complaint
             }
         ],
-        at: [list of secondary keywords],
-        im: [list of im keywords that Portal should respond to with a modal]
+        at_mention: [list of secondary keywords],
     }
 }```
 
-One last option is to enable/disable passthrough - enabling passthrough will have Portal stop processing messages directed to Portal, while keeping it disabled will have Portal pass all messages on to your bot once it's done. This is disabled by default.
+One last option is to enable/disable passthrough - enabling passthrough will have Portal stop processing messages directed to Portal, while keeping it disabled will have Portal pass all messages on to your bot once it's done. Passthrough is enabled by default.
+ ```{
+     ...
+     passthrough: true,
+     ...
+ }```
 
-Finally, register the plugin and it's features with the Botkit controller using `usePlugin()`
-
+Finally, initialize the plugin and install into your app's Botkit controller:  
+Initialize the plugin: 
 ```javascript
-let portal = new Portal({
-    uri: 'https://your_app_listener_instance.com/',
+// import
+import { BotkitPortalPlugin } from 'botkit-portal-slack'
+// or
+let { BotkitPortalPlugin } = require('botkit-portal-slack)
+
+let portal = new BotkitPortalPlugin({
+    receiver_url: 'https://your_app_URI/', 
     portal_token: 'token_from_portal_setup', // DO NOT commit the actual token into your source code
     client_secret: 'other_token_from_portal, // DO NOT commit this token either.
     commands: {see above}
 });
+```
+NOTE - the receiver URL should be the same base URL that you registered with Slack to receive Event API calls. Do not include '/slack/receive' or any other secondary routes, as portal will set up its own TLS 1.2 route for handling cross-platform messages.
 
-controller.usePlugin(portal);
+To install the plugin on bots using Botkit 4.*:
+
+```
+controller = new Botkit.slackbot(config)
+
+controller.usePlugin(portal)
+
 ```
 
+To install the plugin on bots using legacy Botkit (v. 0.7.* and below):
 
-Once registered, Botkit will automatically integrate Portal into your bot, where it will listen for support/helpdesk/feedback requests. All of the plugin's methodswill be available at `controller.plugins.portal`.
+```
+controller = new Botkit.slackbot(config)
+
+portal.init(controller)
+```
+
+Once registered, Botkit will automatically integrate Portal into your bot, where it will listen for support/helpdesk/feedback requests and pass support conversations back and forth between your app and your Slack workspace. 
+
--- src/portal.ts
+++ src/portal.ts
@@ -8,22 +8,28 @@
 
 
 // this is what we're publicly exposing as the plugin
-import { Botkit, BotWorker} from "Botkit"
+import { Botkit, BotWorker } from "Botkit"
 import axios, { AxiosRequestConfig, AxiosPromise } from 'axios';
-import { SlackBotWorker, SlackAdapter } from "botbuilder-adapter-slack";
-import { lodash as _ } from 'lodash'
+import { SlackBotWorker, SlackAdapter, SlackDialog } from "botbuilder-adapter-slack";
+import _ = require('lodash')
+import moment = require('moment');
+import { createCipheriv, randomBytes } from 'crypto'
+import random = require('randomstring');
 
 export class BotkitPortalPlugin {
 
     private __config: any
     private __controller: Botkit
-    private __botversion: any
-    private __reciever_url: string
+    private __botversion: string
+    private __is_legacy: boolean
+    private __receiver_url: string
     private __listener_url: string
+    private __usage: any
+    private __passthrough: boolean
 
     public name: string = 'Slack Portal Plugin'
-    private LISTENER_URL: string = 'https://portal.convobox.com' //have a hard-coded version
-    private ACTION_ID: string = 'portal_support_modal_launch'
+    private LISTENER_URL: string = 'https://app.portalforslack.com/portal/intake' //have a hard-coded version
+    private PORTAL_CALLBACK_ID: string = 'action_portal_launch'
 
     /**
      * config should include:
@@ -46,8 +52,8 @@ export class BotkitPortalPlugin {
         if (config.controller) {
             this.process_controller(config.controller)
         }
-        if (! config.reciever_url) {
-            throw new Error('config.reciever_url missing: Need Slack app base URL to recieve validate incoming responses from the Portal app') //there's a reciever url???
+        if (! config.receiver_url) {
+            throw new Error('config.receiver_url missing: Need Slack app base URL to recieve validate incoming responses from the Portal app') //there's a reciever url???
         }
         if (! config.portal_token) {
             throw new Error('config.portal_token missing: Need an authentication token from the Portal app')
@@ -55,50 +61,97 @@ export class BotkitPortalPlugin {
         if (! config.client_secret) {
             throw new Error('config.client_token missing: Need a client verification token to validate incoming responses from the Portal app')
         }
+        // TODO Add a modal/dialog listener to directly handle existing dialogs
         if (! config.listeners) {
             let keywords = ['support','help','helpdesk','feedback']
             config.listeners = {
+                keywords,
                 slash: {
                     feedback: null,
                     support: null,
                     help: null,
                     helpdesk: null
                 },
-                at: keywords,
-                im: ['im_portal_launch']
+
+                at_mention: keywords,
+                actions: [this.PORTAL_CALLBACK_ID]
             }
-            //create default listeners
+            //create default listeners if nothing passed
+        }
+        this.__receiver_url = config.receiver_url
+        this.__listener_url = config.listener_url ? config.listener_url : this.LISTENER_URL
+        
+        if (config.passthrough) {
+            this.__passthrough = config.passthrough
+        } else {
+            this.__passthrough = true
         }
         if (! config.actions) {
-            config.actions = [this.ACTION_ID]
+            config.actions = [{this.PORTAL_CALLBACK_ID]
+        }
+        this.__usage = {
+            workspaces: [], //need # workspaces for billing/compare w/active users
+            slash: {t: 0, p: 0}, 
+            at: {t: 0, p: 0},
+            im: {t: 0, p: 0, c:0},
+            dialog: {t: 0, p: 0, c:0}
         }
         this.__config = config;
     }
 
+    // private get_usage(): any {
+    //     let key = random.generate()
+    //     let out = []
+    //     //randomly encrypt/anonymize workspace ids
+    //     for ( let k of this.__usage.workspaces) { 
+    //         let cipher = createCipheriv('aes-256-cbc', Buffer.from(key), key);
+    //         let encrypted = cipher.update(k);
+    //         encrypted = Buffer.concat([encrypted, cipher.final()])
+    //         let data = encrypted.toString('hex')
+    //         out.push(data)
+    //     }
+    //     let data_block = Object.assign({}, this.__usage)
+    //     data_block.workspaces = out
+    //     return data_block
+    // }
+
     private process_controller(controller: Botkit): void {
         this.__controller = controller
-        this.__botversion = controller.version
+        this.__is_legacy = typeof controller.version == 'function'
     }
 
+    // NOTE - for Bolt apps, can use init, then app.use(middleware)
+    // BUT, cannot get router (private), so I can't add a listener endpoint
+    // DARN...
     /**
      * 
      * @param botkit : requires an initialized botkit instance
      */
     public init(botkit: Botkit): void {
         this.process_controller(botkit)
-        this.__controller.addDep('portal')
-        const router = botkit.webserver
+        // console.log(`Botkit version: ${this.__botversion}`)
+        console.log('is_legacy? : ', this.__is_legacy)
+        if (!this.__is_legacy) {
+            this.__controller.addDep('portal')
+        }
+        const router = botkit.webserver //THIS MIGHT NOT WORK FOR LEGACY
         // TODO verify that bot has necessary scopes (?) not sure this is necessary as we probably should just be using DMs?
         //  -- this depends on how the origin app interacts with users - if it's DM-heavy, we'll need an alternative
         // TODO need to figure out how to  initialize a handshake with the Portal servers/ handle billing info
         // ============ adds route for handling customer => client comms (passbacks) =========
-        router.post(`${this.__reciever_url}/portal/update`, this.process_passback)
-        if ( this.__botversion >=4 ) {
+        router.post(`/portal/update`, this.process_passback)
+        //TODO figure out usage stats - need to understand overall usage v/ portal usage, # of teams
+        if (!this.__is_legacy) {
             botkit.addPluginExtension('portal', this) //TODO I might just do this manually to keep people from exposing middlewares
+            this.__controller.completeDep('portal')
         } else {
             botkit.middleware.receive.use(this.process_incoming_message)
         }
-        this.__controller.completeDep('portal')
+        // TODO send auth call to portal (?)
+        // the reasonable way would be to listen to incoming messages, log the associated workspace/enterprise, and append to a tracker in controller
+        // then I could send an updated list to portal when new ones pop up
+        // also let me see what proportion of messages involve support
+        console.log('portal_install_complete')
     }
 
     public middlewares(botkit: Botkit): object {
@@ -107,56 +160,137 @@ export class BotkitPortalPlugin {
         }
     }
 
+    // private process_workspace_data(bot, message): void {
+    //     let workspace_id
+    //     try{
+    //         workspace_id = bot.config.id
+    //     } catch (err) {
+    //         workspace_id = bot.getConfig('id') //hope this works!
+    //     }
+    //     if (!this.__usage.workspaces.includes(workspace_id)) {
+    //         this.__usage.workspaces.push(workspace_id)
+    //     }
+    // }
+    private async respond_at(bot: SlackBotWorker, message:any): Promise<void>  {
+        let keywords = this.__config.listeners.at_mention
+        if (!keywords || keywords.length() <=0) // continue to process
+        if (!message.text || message.text.length() <= 0) // continue to process, only modal option
+        if (message.type == 'direct_mention') {
+            if (message.text)message.text.split(' ')[0]
+        }
+        let action_word = 
+        if (message.type == 'mention') {
+
+        }
+
+    }
+
+    private send_to_portal(data, type) {
+
+    }
     /**
      * 
      * @param bot 
      * @param message 
      * @param next 
      */
-    private process_incoming_message(bot, message, next): void {
-        let callback_id = ('callback_id' in message)? message.callback_id.toString() : null;
-        switch (message.type) {
-            case 'interactive_message_callback':
-            case 'interactive_message':
-                callback_id = message.callback_id.toString()
-                let action_id = message.actions[0].name //compare against config
-                let trigger = message.trigger_id
-                break;
-            case 'block_actions':
-                break;
-            case 'slash_command':
-                let slash = this.__config.listeners.slash
-                //get command
-                let command = message.command
-                //check if there's a secondary command
-                let split = (message.text && message.text.length() > 0) ? message.text.split(' ') : []
-                let secondary =  split.length() > 0 ? split[0] : null
-                //test if the main slash command is in listeners
-                if (!(command.toLowerCase() in slash.keys())){
-                    next()
+    private async process_incoming_message(bot: SlackBotWorker, message: any, next: Function): Promise<void> {
+        //@ts-ignore working with 2 versions of botkit
+        let token = bot.getConfig ? bot.getConfig('token') : bot.config.token ? bot.config.token : bot.config.bot.token
+        let type = message.type
+        let listners = this.__config.listeners
+        switch (type) {
+            // verify and get responses to ticket thread and pass on to portal
+            case 'direct_message':
+                // is parent - don't block other messaging interactions w/app
+                if(!message.thread_ts || message.thread_ts == message.ts) return next()
+                try {
+                    let parent = await bot.api.conversations.history({
+                        token: token,
+                        channel: message.channel,
+                        latest: message.thread_ts,
+                        limit: 1,
+                        inclusive: true
+                    })
+                    let ticket_id = this.get_ticket_id(parent)
+                    if (ticket_id) {
+                        // send message to portal.
+                    }
                 }
-                // test if there's a secondary listener to pay attention to
-                let secondary_listeners = slash[command];
-                if (secondary_listeners && !(secondary in _.castArray(secondary_listeners))) {
-                    next()
-                }
-                primary = split.shift()
-                break;
-            case 'modal':
-                break;
-            case 'modal_submission':
-            case 'dialog_submission':
-                callback_id = message.callback_id.toString()
-                bot.dialogOk()
-                break;
             case 'direct_mention':
+            case 'mention': 
+                if (!listners.at_mention) break;
+                await this.respond_at(bot, message)
+                next()
+                // probably treat the same 
+                // get direct mention
+                // verify that they're asking for support
+                // respond with ephemeral message / block with button
+                // ephemeral allows user to just send, or to edit and then send (triggers dialog)
+                break;
+            case 'message_action':
+            case 'slash_command':
+                // get message.trigger_id, launch dialog, and get the dialog response.
+                break;
+            case 'interactive_message_callback': // handle IM actions (legacy)
+            case 'block_actions': // handle block actions
+                if (!message.callback_id) return
+
+                // ignore if not from a dialog
+                if (message.view && (message.view.type == 'modal' || message.view.type == 'home')) return;
+                // if message.view && message.view.type == 'home' or 'modal' handle as modal/home
+                // I guess this is where you'd see updates in modal view?
+                // this shouldn't mess anything up - I'm moving the support message to an IM or home page anyways.
+            // case 'message_action': // handle action dropdown? doesn't seem to work
+            case 'view_submission':
+                bot.
+                if (message.private_metadata != '???') return
+                let values = message.state.values
+
+                //validate that there's text in the text input
+                let text = ''
+                if (!text) {
+                    bot.dialogError(
+                    "response_action": "errors",
+                    "errors": {
+                      "ticket-due-date": "You may not select a due date in the past"
+                    }
+                  }
+            case 'view_closed':
+                // check for is_cleared flag: if cleared, user used x rather than cancel
+            // ignoring these - legacy
+            case 'dialog_cancellation': // would get callback_id
+            case 'dialog_submission': // would get a callback_id and specified submission.* field(s)
+                //ignore
                 break;
         }
-        //get message type
-        //
 
-        // only call next if message doesn't need to be intercepted...
-        next();
+    }
+
+
+    private get_ticket_id(message: any): String {
+        if (message.ticket_id) return message.ticket_id
+        try {
+            let blocks = message.blocks
+            let id_block = _.find(blocks, (i) => ('block_id' in i && i.block_id.startsWith('portal_ticket:')))
+            if (id_block) {
+                return id_block.block_id
+            }
+            return null
+        } catch (err) {
+            return null
+        }
+
+    }
+
+
+    private launch_support_dialog(type:string, message:any): any {
+        let dialog = new SlackDialog('title', 'callback_id', 'submit_label', ['elements'])
+        dialog.addTextarea('label','name','value',{}, 'subtype') //add in text area with optional text
+        dialog.addSelect('label', 'name', 'value(string|number|object', [{label: 'option_value', value:'option_value'}])
+        dialog.notifyOnCancel(true)
+        dialog.callback_id('')//set up 
+        this.__config.replyWithDialog(dialog.asObject())
     }
 
 
@@ -173,10 +307,11 @@ export class BotkitPortalPlugin {
      */
     private async process_passback(req: any, res: any): Promise<void> {
         if (!req.body) return res.status(400).send('Bad Request');
+        if (! this.verify_passback(req)) return res.status(401).send('unauthorized')
         let { token, message, hash, timestamp } = req.body //restructure to mirror botkit message structure
-        if (! await this.verify_passback(req)) {
-            return res.status(401).send('Unauthorized')
-        }
+        // if (! await this.verify_passback(req)) {
+        //     return res.status(401).send('Unauthorized')
+        // }
         if (! message) return res.status(400).send('Bad Request: no message body');
         res.status(201).send(hash) //initial confirmation to turn off bot thread and let it wait/move wait to redis
         let status = await this.process_client_update(message)  //verify that the client response was sent and recieved correctly
@@ -241,3 +376,4 @@ export class BotkitPortalPlugin {
         })
     }
 }
+
