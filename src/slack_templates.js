0076e02e6b5ba48b0ccce986131a2e176de0efd8
Seth Stradling <seth.g.stradling@gmail.com>
2020-01-15 16:51:57 -0700

    fixing messaging bugs

--- src/slack_templates.js
+++ src/slack_templates.js
@@ -1,11 +1,11 @@
 console.log('loading_templates')
 const _ = require('lodash')
 const { block, element, object, view, TEXT_FORMAT_MRKDWN } = require('slack-block-kit')
-const { text, confirm, option, optionGroup, optionGroups, plainTextInput } = object
+const { text, confirm, option, optionGroup, optionGroups } = object
 const {  
     button, overflow, staticSelect, externalSelect, 
     usersSelect, conversationsSelect, channelsSelect,
-    datePicker
+    datePicker, plainTextInput
   } = element
 
 const { section, actions, divider, context, image, input } = block
@@ -34,22 +34,24 @@ module.exports = {
     },
 
     // ignoring select until we have the rest worked out
-    support_modal: (text, metadata) => {
+    support_modal: (input_text, metadata, callback_id) => {
 
         // let input_block = (actionId, {placeholderText, initialValue, multiLine, minLength, maxLength})
         let input_params = {
             placeholderText: `Enter your support request here`,
             multiline: true,
-            minLength: 3
+            // minLength: 3
         }
 
-        if (text) input_params.initial_value = text
         let input_element = plainTextInput('input_text', input_params)
+        input_element.multiline = true
+        if (input_text) input_element.initial_value = input_text
 
         let input_block = input(`Feel free to send us a question, concern, or other feedback in the field below. Thanks!`, input_element, {
             // hintText, blockId
         })
 
+        //TODO pull this from keywords/types
         let options = [
             option('Support','support'),
             option('Feedback','feedback'),
@@ -60,12 +62,13 @@ module.exports = {
         }))
 
         let blocks = [ input_block ]
+        // blocks.push(type_select_block)
 
         let report_modal = view.modal('Contact Support', blocks, {
             closeText: 'Cancel',
             submitText: 'Submit Request',
             privateMetadata: metadata,
-            // callbackId: 'my-callback-id',
+            callbackId: callback_id,
             clearOnClose: true,
             notifyOnClose: true,
             // externalId: 'unique-external-id',
@@ -74,28 +77,28 @@ module.exports = {
         return report_modal
     },
 
-    dm_response: (keyword=null, text=null, type='support') => {
+    dm_response: (keyword=null, input_text=null, type='support') => {
         let message_text = `Hi! Do you want to`
         if (keyword) {
             message_text = `Hi :wave: We heard you say \`${keyword}\` - do you want to`
             edit_text = 'Send Request'
         }
-        if (text) message_text += ` send the following support request to our team?`
+        if (input_text) message_text += ` send the following support request to our team?`
         else message_text += ` open a new support request?`
 
         let blocks = [section(text(message_text, TEXT_FORMAT_MRKDWN))]
-        if (text) blocks.push(section(text(`>${text}`,TEXT_FORMAT_MRKDWN)))
+        if (input_text) blocks.push(section(text(`>${input_text}`,TEXT_FORMAT_MRKDWN)))
 
         let msg_actions = [
-            button('action_portal_cancel', 'Cancel', {value=`${text||'no_msg'}`})//always cancel opt
+            button('action_portal_cancel', 'Cancel', {value:`${input_text||'no_msg'}`})//always cancel opt
         ]
 
-        let create_btn = button(`action_portal_launch:${type||'support'}`, 'Open New Request', {value:`${text||'no_msg'}`, style:'primary'})
-        let edit_btn = button(`action_portal_launch:${type||'support'}`, 'Edit Request', {value:`${text||'no_msg'}`, style:'primary'})
-        let submit_btn = button(`action_portal_send:${type||'support'}`, 'Send Request', {value:`${text||'no_msg'}`})
+        let create_btn = button(`action_portal_launch:${type||'support'}`, 'Open New Request', {value:`${input_text||'no_msg'}`, style:'primary'})
+        let edit_btn = button(`action_portal_launch:${type||'support'}`, 'Edit Request', {value:`${input_text||'no_msg'}`, style:'primary'})
+        let submit_btn = button(`action_portal_send:${type||'support'}`, 'Send Request', {value:`${input_text||'no_msg'}`})
 
-        if (text) {
-            _.concat(msg_actions, edit_btn, submit_btn)
+        if (input_text) {
+            msg_actions = _.concat(msg_actions, edit_btn, submit_btn)
         } else {
             msg_actions.push(create_btn)
         }
